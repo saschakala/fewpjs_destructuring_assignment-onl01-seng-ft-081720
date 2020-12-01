f6b10d5ebe989497e2609665470db1b88039eaab
“saschakala” <mxsaschakala@gmail.com>
2020-12-01 16:36:29 -0500

    commit tree 15cddb17ec343696d7a5e86734519cc7f597396b

--- index.js
+++ index.js
@@ -25,19 +25,38 @@ const k_muppet = {
   k_partner: 'Miss Piggy'
 };
 
+const [moo, neigh, baa, oink, cluck] = farmAnimals.split(' ');
+
+const [Bessie, ,Dolly, Babe, Little] = farmAnimals.split(' ');
+
+const [black_and_white, , black, pink, ] = farmAnimals.split(' ');
+
+const [red, orange, yellow, green, blue, indigo, violet] = colors;
+
+const [r, o, y, g, b, , v] = colors;
+
+const [, , , , , indg, ] = colors;
+
+const {name, color, song, job, partner} = muppet;
+
+const {k_job, k_partner} = k_muppet;
+
+const {song_2,song_4} = k_muppet.k_album.the_muppet_movie; 
+
+
+
+
+
+
+
 // Strings
 
-// 1. Use destructuring to assign appropriate variables based on the sounds animals make.
-
-// 2. Bolt the horse wandered off, so just give us four animals, and let's name them Bessie, Dolly, Babe, and Little.
-
-// 3. Little the chicken had to go back to the coop, so now we're left with three. Let's use color variables of black_and_white, black, and pink.
 
 // Arrays
 
-// 4. Use destructuring to assign appropriate variables using the color names.
 
-// 5. Some people have a really hard time picking out indigo, so let's leave that one out, using the first letter of each color as the variable names.
+
+
 
 // 6. But wait! Indigo is now feeling *super* left out. Let's only assign indigo using indg. 
 
